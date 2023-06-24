d34293e4724967b1004af720c3c512235d1a7325
Zeit241 <easywin.ru@gmail.com>
2023-06-25 00:11:54 +0300

    change loading component

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/app/(components)/ui/navbar";
 import ReactWrapper from "@/app/(components)/record-wrapper";
 import { Metadata } from "next";
 import { Suspense } from "react";
+import RecordLoading from "@/app/(components)/record-loading";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -13,7 +14,7 @@ export default function DashboardPage() {
       <Navbar username="John Doe" main_url={process.env.MAIN_URL!.toString()} />
       <div className={"p-8"}>
         <h1 className={"text-2xl mb-4"}>Your projects</h1>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<RecordLoading />}>
           {/* @ts-expect-error Async Server Component */}
           <ReactWrapper />
         </Suspense>
