da99c90491f1a9577a23de17c8b52aa32460524e
Zeit241 <easywin.ru@gmail.com>
2023-07-05 16:17:53 +0300

    Add-card component: new functions
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/add-card.tsx
+++ src/app/(components)/add-card.tsx
@@ -13,10 +13,9 @@ import {
 import { Input } from "@/app/(components)/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import {
   Form,
-  FormControl,
   FormDescription,
   FormField,
   FormItem,
@@ -24,7 +23,10 @@ import {
 } from "@/app/(components)/ui/form";
 import { Card } from "@/app/(components)/ui/card";
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import VerifyRecordName from "@/app/server/verify-record-name";
+import CreateRecord from "@/app/server/create-record";
+import { useRouter } from "next/navigation";
 
 const createRecordSchema = z.object({
   name: z
@@ -39,25 +41,49 @@ const createRecordSchema = z.object({
       message: "Invalid characters.",
     }),
 });
-export default function CreateCardBtn() {
+export default function CreateCardBtn({ id }: { id: string }) {
+  const router = useRouter();
   const [isNameChecked, setIsNameChecked] = useState<boolean>(false);
   const [name, setName] = useState<string>("");
   const [nameError, setNameError] = useState<boolean>(false);
   const form = useForm<z.infer<typeof createRecordSchema>>({
+    mode: "onChange",
+    reValidateMode: "onChange",
     resolver: zodResolver(createRecordSchema),
     defaultValues: {
       name: "",
     },
   });
 
-  async function verifyName() {
-    setTimeout(() => {
+  useEffect(() => {
+    if (name.length > 0) {
+      setIsNameChecked(false);
+      const timeOutId = setTimeout(() => verifyName(name), 500);
+      return () => clearTimeout(timeOutId);
+    }
+  }, [name]);
+
+  async function verifyName(name: string): Promise<boolean> {
+    const is_name_taken = await VerifyRecordName({ name });
+    if (is_name_taken.status === 403) {
+      setNameError(true);
+      form.setError("name", { message: "Name already taken" });
+    } else {
+      setNameError(false);
       setIsNameChecked(true);
-    }, 1500);
+    }
+
+    return true;
   }
 
-  function onSubmit(values: z.infer<typeof createRecordSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof createRecordSchema>) {
+    setIsNameChecked(false);
+
+    const res = await CreateRecord({ name, id });
+
+    if (res.status == 200) {
+      router.push(`dashboard/add/${name}`, {});
+    }
   }
 
   return (
@@ -79,7 +105,11 @@ export default function CreateCardBtn() {
             </DialogDescription>
           </DialogHeader>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              onChange={() => setName(form.getValues().name)}
+              className="space-y-8"
+            >
               <FormField
                 control={form.control}
                 name="name"
@@ -87,24 +117,48 @@ export default function CreateCardBtn() {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <div className={"flex flex-row items-center "}>
-                      <Card
-                        className={
-                          "border-r-0 w-[120px] items-center flex py-2 px-3 rounded-r-none text-sm h-10 pr-0 leading-none"
-                        }
-                      >
-                        {"prettylinks.com/"}
-                      </Card>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          autoComplete={"off"}
-                          className={"border-l-0 rounded-l-none pl-0 "}
-                          placeholder="name"
-                          spellCheck={"false"}
-                          type="text"
-                          //onChange={(e) => console.log(e.target.value)}
-                        />
-                      </FormControl>
+                      <Controller
+                        name="name"
+                        control={form.control}
+                        render={({
+                          field: { onChange },
+                          fieldState: { error },
+                        }) => (
+                          <div className={"flex-col flex w-[95%]"}>
+                            <div className={"flex flex-row"}>
+                              <Card
+                                className={`border-r-0 w-[120px] items-center flex py-2 px-3 rounded-r-none text-sm h-10 pr-0 leading-none ${
+                                  error
+                                    ? "border-red-500"
+                                    : isNameChecked
+                                    ? "border-green-500"
+                                    : ""
+                                }`}
+                              >
+                                {"prettylinks.com/"}
+                              </Card>
+                              <Input
+                                {...field}
+                                autoComplete={"off"}
+                                className={`border-l-0 rounded-l-none pl-0 ${
+                                  error
+                                    ? "border-red-500"
+                                    : isNameChecked
+                                    ? "border-green-500"
+                                    : ""
+                                }`}
+                                onChange={onChange}
+                                placeholder="name"
+                                spellCheck={"false"}
+                                type="text"
+                              />
+                            </div>
+                            <span className={"text-red-500 flex flex-col"}>
+                              {error?.message}
+                            </span>
+                          </div>
+                        )}
+                      />
                     </div>
                     <FormDescription>
                       This is your public display name. It can be changed later.
@@ -114,10 +168,11 @@ export default function CreateCardBtn() {
               />
               <DialogFooter>
                 <Button
+                  className={"select-none"}
                   type="submit"
-                  disabled={name === "" || !isNameChecked || nameError}
+                  disabled={!isNameChecked || nameError || name.length < 5}
                 >
-                  {name === "" ? null : isNameChecked ? null : (
+                  {isNameChecked || nameError || name.length < 5 ? null : (
                     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                   )}
                   Create
