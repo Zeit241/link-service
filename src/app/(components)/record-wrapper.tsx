6f2ed44622e15676d2170b1725a61e1d9b450181
Zeit241 <easywin.ru@gmail.com>
2023-07-07 22:28:25 +0300

    fix types and update query
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/record-wrapper.tsx
+++ src/app/(components)/record-wrapper.tsx
@@ -6,6 +6,10 @@ import { authOptions } from "@/lib/auth";
 import CreateCardBtn from "@/app/(components)/add-card";
 import { redirect } from "next/navigation";
 
+type customType = Record & { Link: { id: string }[] } & {
+  Statistic: { id: string }[];
+};
+
 export default async function RecordWrapper() {
   const session = await getServerSession(authOptions);
   if (session) {
@@ -13,10 +17,22 @@ export default async function RecordWrapper() {
       where: {
         userId: session?.user?.id,
       },
+      include: {
+        Link: {
+          include: {
+            _count: true,
+          },
+        },
+        Statistic: {
+          select: {
+            id: true,
+          },
+        },
+      },
     });
     return (
       <div className={"flex flex-row flex-wrap gap-4"}>
-        {data.map((record: Record) => (
+        {data.map((record: customType) => (
           <RecordCard key={record.id} record={record} />
         ))}
         <CreateCardBtn id={session?.user?.id} />
