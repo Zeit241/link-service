c0fb5de5b4c5d5f3fdbf68112772b3eda18bf70c
Zeit241 <easywin.ru@gmail.com>
2023-07-07 22:49:21 +0300

    improved component design
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/phone-frame.tsx
+++ src/app/(components)/phone-frame.tsx
@@ -1,9 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { Loader2 } from "lucide-react";
+import { Skeleton } from "@/app/(components)/ui/skeleton";
+
 export default function PhoneFrame({ id }: { id: string }) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {}, []);
   return (
-    <div className={"w-[320px] h-[620px] border-[15px] rounded-2xl m-12"}>
-      <iframe className={"w-full h-full"} src={`/${id}`}></iframe>
+    <div
+      className={
+        "w-[320px] h-[620px] border-[15px] rounded-[50px] m-12 overflow-hidden"
+      }
+    >
+      {!isLoading && (
+        <Skeleton className={"w-full h-full flex items-center justify-center"}>
+          <Loader2 size={42} className={"animate-spin"} />
+        </Skeleton>
+      )}
+
+      <iframe
+        className={`w-full rounded-3xl h-full ${!isLoading ? "hidden" : ""}`}
+        src={`/${id}`}
+        onLoad={() => setIsLoading(true)}
+      ></iframe>
     </div>
   );
 }
