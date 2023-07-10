f761e1a1496bf19e5037063a8ceabfcc08b02264
Zeit241 <troller1945@gmail.com>
2023-07-10 23:38:19 +0300

    reworked record component

--- src/app/(components)/record-card.tsx
+++ src/app/(components)/record-card.tsx
@@ -4,90 +4,99 @@ import { Card } from "@/app/(components)/ui/card";
 import { useRouter } from "next/navigation";
 import { Badge } from "@/app/(components)/ui/badge";
 import { Separator } from "@/app/(components)/ui/separator";
-import { useRef } from "react";
+import {Switch} from "@/app/(components)/ui/switch";
+import {Trash} from "lucide-react";
+import {useState} from "react";
+import UpdateRecord from "@/app/server/update-record";
+import {deleteRecord} from "@/app/server/delete-record";
+import {AlertDialog, AlertDialogTrigger} from "@radix-ui/react-alert-dialog";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription, AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from "@/app/(components)/ui/alert-dialog";
 
-type customType = Record & { Link: { id: string }[] } & {
+type unionRecordType = Record & { Link: { id: string }[] } & {
   Statistic: { id: string }[];
 };
-export default function RecordCard({ record }: { record: customType }) {
-  const delete_btn = useRef<HTMLDivElement>(null);
+export default function RecordCard({ record }: { record: unionRecordType }) {
   const router = useRouter();
+  const [recordEnabled, setRecordEnabled] = useState<boolean>(record.enabled)
   const navigateToPage = (event: React.MouseEvent) => {
-    // if (
-    //   event.currentTarget.id !== delete_btn?.current?.id ||
-    //   event.currentTarget.parentElement?.id !== delete_btn?.current?.id
-    // ) {
-    //   console.log("REDIRECT");
     return router.push(`dashboard/modify/${record.url}`);
-    // }
   };
 
   return (
     <>
       <Card
-        onClick={(e) => navigateToPage(e)}
+
         className={
-          "flex flex-col p-5 cursor-pointer min-h-[175px] hover:bg-accent"
+          "flex flex-col p-5 min-h-[175px] hover:bg-slate-900"
         }
       >
-        <div className={"flex flex-row items-center justify-between mb-1"}>
-          <h1 className={"text-2xl font-bold uppercase flex flex-wrap"}>
-            {record.name}
-          </h1>
-          <div>
-            {/*<AlertDialog>*/}
-            {/*  <AlertDialogTrigger>*/}
-            {/*    <div*/}
-            {/*      className={"p-1 pr-3 pl-3 pt-3"}*/}
-            {/*      ref={delete_btn}*/}
-            {/*      id={"delete_btn"}*/}
-            {/*    >*/}
-            {/*      <Trash className="h-4 w-4" />*/}
-            {/*    </div>*/}
-            {/*  </AlertDialogTrigger>*/}
-            {/*  <AlertDialogContent>*/}
-            {/*    <AlertDialogHeader>*/}
-            {/*      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>*/}
-            {/*      <AlertDialogDescription>*/}
-            {/*        This action cannot be undone. This will permanently delete*/}
-            {/*        your project and remove your data from our servers.*/}
-            {/*      </AlertDialogDescription>*/}
-            {/*    </AlertDialogHeader>*/}
-            {/*    <AlertDialogFooter>*/}
-            {/*      <AlertDialogCancel>Cancel</AlertDialogCancel>*/}
-            {/*      <AlertDialogAction*/}
-            {/*        onClick={async () => await deleteRecord(record.id)}*/}
-            {/*      >*/}
-            {/*        Continue*/}
-            {/*      </AlertDialogAction>*/}
-            {/*    </AlertDialogFooter>*/}
-            {/*  </AlertDialogContent>*/}
-            {/*</AlertDialog>*/}
+        <div
+            className={"flex flex-col w-full cursor-pointer"}
+            onClick={(e) => navigateToPage(e)}
+        >
+          <div className={"flex flex-row items-center justify-between mb-1"}>
+            <h1 className={"text-2xl font-bold uppercase flex flex-wrap"}>
+              {record.name}
+            </h1>
+            <div>
+            </div>
           </div>
-        </div>
-        <span className={"text-sm text-foreground "}>
+          <span className={"text-sm text-foreground "}>
           {record.description || "May be you can add a description"}
         </span>
-        <Separator className={"m-2.5"} orientation="horizontal" />
-        <>
-          <div className={"flex flex-row items-center gap-3"}>
-            <Badge variant={record.enabled ? "secondary" : "destructive"}>
-              {record.enabled ? "Enabled" : "Disabled"}
-            </Badge>
-            <Badge
-              variant={record.Link.length > 0 ? "secondary" : "destructive"}
-            >
-              {record.Link.length + " links"}
-            </Badge>
-            <Badge
-              variant={
-                record.Statistic.length > 0 ? "secondary" : "destructive"
-              }
-            >
-              {record.Link.length + " clicks"}
-            </Badge>
-          </div>
-        </>
+          <Separator className={"m-2.5"} orientation="horizontal" />
+          <>
+            <div className={"flex flex-row items-center gap-2"}>
+              <Badge variant={recordEnabled ? "secondary" : "destructive"}>
+                {recordEnabled ? "Enabled" : "Disabled"}
+              </Badge>
+              <Badge
+                  variant={record.Link.length > 0 ? "secondary" : "destructive"}
+              >
+                {record.Link.length + " links"}
+              </Badge>
+              <Badge
+                  variant={
+                    record.Statistic.length > 0 ? "secondary" : "destructive"
+                  }
+              >
+                {record.Statistic.length + " clicks"}
+              </Badge>
+            </div>
+          </>
+        </div>
+        <div className={"flex flex-row justify-between items-center mt-2"}>
+          <Switch id="record-enabled" checked={recordEnabled} onCheckedChange={async ()=> {
+            await UpdateRecord( record.id, { enabled: !recordEnabled})
+            setRecordEnabled(!recordEnabled)
+          }}/>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Trash size={18} className={" cursor-pointer"}/>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will <span className={"text-red-700"}>delete your</span> {record.url}
+                   project and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction  onClick={async ()=> await deleteRecord(record.id)}>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+
+        </div>
       </Card>
     </>
   );
