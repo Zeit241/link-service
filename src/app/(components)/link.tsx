7489600edea7c5777d21f4d460770c970da64027
Zeit241 <easywin.ru@gmail.com>
2023-07-12 23:43:55 +0300

    redesign link card
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/link.tsx
+++ src/app/(components)/link.tsx
@@ -1,36 +1,30 @@
-"use client";
-import { Link } from "@prisma/client";
-import linkIcon from "@/../public/img/standart-link.svg";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+"use client"
+
+import { useRouter } from "next/navigation"
+import { Link } from "@prisma/client"
 
 type LinkItemProps = {
-  Link: Omit<Link, "createdAt" | "updatedAt">;
-};
+  Link: Omit<Link, "createdAt" | "updatedAt">
+}
 export default function LinkItem({ Link }: LinkItemProps) {
-  const router = useRouter();
+  const router = useRouter()
   const updateLinks = async () => {
     //TODO: Собирать статистику по каждой ссылке
-    return router.push(Link.url, {});
-  };
+    return router.push(Link.url, {})
+  }
 
   return (
     <div
       onClick={updateLinks}
       className={
-        "w-[75%] min-w-[280px] max-w-[560px] h-20 flex rounded-xl bg-amber-500 cursor-pointer"
+        "flex h-auto w-[75%] min-w-[260px] max-w-[560px] cursor-pointer content-center bg-muted p-2.5 hover:scale-105 "
       }
     >
-      <Image
-        src={linkIcon}
-        alt={"Link icon"}
-        width={50}
-        className={"ml-4 mr-4"}
-      />
-      <div className={"flex-1"}>
-        <h1 className={"font-semibold sm:text-base"}>{Link.name}</h1>
-        <p>Some description</p>
+      <div className={"flex flex-1 items-center justify-center"}>
+        <h1 className={"break-all text-center font-semibold sm:text-base"}>
+          {Link.name}
+        </h1>
       </div>
     </div>
-  );
+  )
 }
