3b63b500d8b684a3317e13b00a67da0e2dd80445
Zeit241 <easywin.ru@gmail.com>
2023-07-13 23:40:48 +0300

    cleanup
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(authenticated)/dashboard/modify/[slug]/page.tsx
+++ src/app/(authenticated)/dashboard/modify/[slug]/page.tsx
@@ -1,28 +1,24 @@
 import * as React from "react"
-import {notFound} from "next/navigation"
+import { notFound } from "next/navigation"
 
-import {useStore, useStoreTest} from "@/lib/storage/storage"
 import ModifyLinksWrapper from "@/app/(components)/modify-links-wrapper"
 import StoreInitializer from "@/app/(components)/store-initalizer"
-import {GetRecordLinks} from "@/app/server/get-record-links"
+import { GetRecordLinks } from "@/app/server/get-record-links"
 
 export default async function ModifyPage({
-                                             params,
-                                         }: {
-    params: { slug: string }
+  params,
+}: {
+  params: { slug: string }
 }) {
-    const record = await GetRecordLinks(params.slug)
-
-    if (!record) {
-        return notFound()
-    } else {
-        useStore.setState({record: record, links: record.Link})
-        console.log(useStoreTest.getState())
-        return (
-            <>
-                <StoreInitializer data={record}/>
-                <ModifyLinksWrapper record={record}/>
-            </>
-        )
-    }
+  const record = await GetRecordLinks(params.slug)
+  if (!record) {
+    return notFound()
+  } else {
+    return (
+      <>
+        <StoreInitializer data={record} />
+        <ModifyLinksWrapper record1={record} />
+      </>
+    )
+  }
 }
