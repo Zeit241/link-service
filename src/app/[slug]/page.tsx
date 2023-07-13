1bdd16b53aea3762abaae5fcd1d40c012a79ff18
Zeit241 <easywin.ru@gmail.com>
2023-07-13 23:45:37 +0300

    fixed param's missed
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,37 +1,36 @@
-import {Suspense} from "react"
-import {Metadata} from "next"
-import {notFound} from "next/navigation"
+import { Suspense } from "react"
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
 
 import RecordPage from "@/app/(components)/record"
-import {GetRecordLinks} from "@/app/server/get-record-links"
+import { GetRecordLinks } from "@/app/server/get-record-links"
 
 export async function generateMetadata({
-                                           params,
-                                       }: {
-    params: { slug: string }
+  params,
+}: {
+  params: { slug: string }
 }): Promise<Metadata> {
-    return {
-        title: `@${params.slug} | Pretty Links`,
-    }
+  return {
+    title: `@${params.slug} | Pretty Links`,
+  }
 }
 
 export default async function RecordsPage({
-                                              params,
-                                          }: {
-    params: { slug: string }
+  params,
+}: {
+  params: { slug: string }
 }) {
-    const data = await GetRecordLinks(params.slug)
+  const data = await GetRecordLinks(params.slug)
 
-    if (!data) {
-        //redirect("/404");
-        return notFound()
-    }
+  if (!data) {
+    return notFound()
+  }
 
-    return (
-        <div className={"overflow-x-hidden"}>
-            <Suspense fallback={<div>Loading...</div>}>
-                <RecordPage data={data}/>
-            </Suspense>
-        </div>
-    )
+  return (
+    <div className={"overflow-x-hidden"}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RecordPage data={data} links={data.Link} />
+      </Suspense>
+    </div>
+  )
 }
