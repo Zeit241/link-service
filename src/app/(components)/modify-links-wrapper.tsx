46b75bc4bb1fee98218851fda1561ba6df35a029
Zeit241 <easywin.ru@gmail.com>
2023-07-13 23:43:47 +0300

    implemented zustand state manager and added loading animation
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/modify-links-wrapper.tsx
+++ src/app/(components)/modify-links-wrapper.tsx
@@ -5,10 +5,11 @@ import * as React from "react"
 import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Link, Record } from "@prisma/client"
-import { Plus, X } from "lucide-react"
+import { Loader2, Plus, X } from "lucide-react"
 import { Controller, useForm } from "react-hook-form"
 import { z } from "zod"
 
+import { useStore } from "@/lib/storage/storage"
 import ModifyLinkCard from "@/app/(components)/modify-link-card"
 import RecordPage from "@/app/(components)/record"
 import { Button } from "@/app/(components)/ui/button"
@@ -18,45 +19,51 @@ import { Input } from "@/app/(components)/ui/input"
 import { Label } from "@/app/(components)/ui/label"
 import CreateLink from "@/app/server/create-link"
 
+const createNewLink = z.object({
+  url: z.string().refine(
+    (value) => {
+      const pattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+          "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[-*a-z\\d]{2,}|" + // domain name
+          "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
+          "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+          "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+          "(\\#[-a-z\\d_]*)?$", // fragment locator
+        "i"
+      )
+      return pattern.test(punycode.toASCII(value))
+    },
+    { message: "Invalid URL" }
+  ),
+})
 export default function ModifyLinksWrapper({
-  record,
+  record1,
 }: {
-  record: Record & { Link: Link[] }
+  record1: Record & { Link: Link[] }
 }) {
+  const { links, record, modify } = useStore()
   const [isAddCardOpen, setIsAddCardOpen] = useState<boolean>(false)
-  const [url, setUrl] = useState<string>("")
-  const [num, setNum] = useState<number>(0)
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   async function onSubmit(value: z.infer<typeof createNewLink>) {
+    setIsLoading(true)
     const res = await CreateLink({
       url: value.url,
       name: value.url,
       record_id: record.id,
-      record_name: record.name,
-      order: record.Link.length,
+      record_name: record?.name,
+      order: record?.Link?.length,
     })
-    setIsLoading(true)
-    setNum(num + 1)
+
+    if (links !== undefined && links.length > 0) {
+      if (res.link) {
+        modify([...links, res?.link])
+      }
+    }
+    setIsLoading(false)
+    setIsAddCardOpen(false)
   }
 
-  const createNewLink = z.object({
-    url: z.string().refine(
-      (value) => {
-        const pattern = new RegExp(
-          "^(https?:\\/\\/)?" + // protocol
-            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[-*a-z\\d]{2,}|" + // domain name
-            "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
-            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-            "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-            "(\\#[-a-z\\d_]*)?$", // fragment locator
-          "i"
-        )
-        return pattern.test(punycode.toASCII(value))
-      },
-      { message: "Invalid URL" }
-    ),
-  })
   const form = useForm<z.infer<typeof createNewLink>>({
     resolver: zodResolver(createNewLink),
     mode: "onChange",
@@ -65,19 +72,20 @@ export default function ModifyLinksWrapper({
       url: "",
     },
   })
+
   return (
     <>
       <div
-        className={"flex w-full flex-row items-start justify-center p-8 pb-0"}
-      >
+        className={"flex w-full flex-row items-start justify-center p-8 pb-0"}>
         <div className={"flex flex-1 flex-wrap items-center  justify-center"}>
           <div className={"flex w-full flex-col items-center gap-5 pb-4 pt-4"}>
             {!isAddCardOpen && (
               <Button
                 variant="secondary"
                 className="max-w-xl p-4 min-[320px]:w-[95%]"
-                onClick={() => setIsAddCardOpen(true)}
-              >
+                onClick={() => {
+                  setIsAddCardOpen(true)
+                }}>
                 <Plus /> Add new link
               </Button>
             )}
@@ -85,13 +93,11 @@ export default function ModifyLinksWrapper({
               <Card
                 className={
                   "flex max-w-xl flex-col gap-4 p-4 min-[320px]:w-[95%]"
-                }
-              >
+                }>
                 <Form {...form}>
                   <form
                     onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8"
-                  >
+                    className="space-y-8">
                     <div className="">
                       <FormField
                         control={form.control}
@@ -101,16 +107,15 @@ export default function ModifyLinksWrapper({
                             <div
                               className={
                                 "flex w-full flex-row items-center justify-between"
-                              }
-                            >
+                              }>
                               <Label htmlFor={"link"}>Enter URL</Label>
                               <Button
+                                disabled={isLoading}
                                 variant="ghost"
                                 size="icon"
                                 onClick={() => {
                                   setIsAddCardOpen(false)
-                                }}
-                              >
+                                }}>
                                 <X size={18} />
                               </Button>
                             </div>
@@ -130,15 +135,23 @@ export default function ModifyLinksWrapper({
                                       autoCorrect="off"
                                       spellCheck={"false"}
                                       onChange={onChange}
+                                      className={`${isLoading ? "hidden" : ""}`}
                                     />
                                   </>
                                 )}
                               />
                               <Button
-                                disabled={!!form.formState.errors.url?.message}
-                                type={"submit"}
-                              >
-                                Add
+                                disabled={
+                                  !!form.formState.errors.url?.message ||
+                                  isLoading
+                                }
+                                className={isLoading ? "w-full" : ""}
+                                type={"submit"}>
+                                {isLoading ? (
+                                  <Loader2 className={"animate-spin"} />
+                                ) : (
+                                  "Add"
+                                )}
                               </Button>
                             </div>
                           </FormItem>
@@ -149,28 +162,28 @@ export default function ModifyLinksWrapper({
                 </Form>
               </Card>
             )}
-
-            {record.Link.map((link: Link, index: number, array: Link[]) => {
-              return (
-                <div className={"max-w-xl min-[320px]:w-[95%]"} key={index}>
-                  <ModifyLinkCard
-                    link={link}
-                    index={index}
-                    max={array.length}
-                  />
-                </div>
-              )
-            })}
+            {links && links.length > 0
+              ? links.map((link: Link, index: number, array: Link[]) => {
+                  return (
+                    <div className={"max-w-xl min-[320px]:w-[95%]"} key={index}>
+                      <ModifyLinkCard
+                        link={link}
+                        index={index}
+                        max={array.length}
+                      />
+                    </div>
+                  )
+                })
+              : null}
           </div>
         </div>
 
         <div
           className={
             "sticky top-[22%] mr-6 hidden h-[620px] w-[320px] min-w-[320px] overflow-hidden rounded-[50px] border-[15px] md:block lg:block xl:block"
-          }
-        >
+          }>
           <div className={"h-full w-full overflow-x-hidden "}>
-            <RecordPage data={record} />
+            <RecordPage data={record} links={links} />
           </div>
         </div>
       </div>
