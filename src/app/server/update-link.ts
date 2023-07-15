7673387fc66308dd31eed8859d2a77fa8b194aec
Zeit241 <easywin.ru@gmail.com>
2023-07-15 23:52:44 +0300

    removed the extra code
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/server/update-link.ts
+++ src/app/server/update-link.ts
@@ -1,7 +1,5 @@
 "use server"
 
-import { revalidatePath } from "next/cache"
-
 import { prisma } from "@/lib/database"
 
 interface UpdateLinkProps {
@@ -15,12 +13,12 @@ export default async function UpdateLink(
   id: string,
   data: UpdateLinkProps
 ): Promise<{ status: number; message: string }> {
-  const link = await prisma.link.update({
+  await prisma.link.update({
     where: {
       id: id,
     },
     data: data,
   })
-  revalidatePath("/dashboard/modify/" + data.url)
+
   return { status: 200, message: "Done!" }
 }
