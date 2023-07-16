e9735db74a3c882e26307ecf9e1b17cd868b4449
Zeit241 <easywin.ru@gmail.com>
2023-07-16 21:17:43 +0300

    Update notFound page's design
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,43 @@
-//TODO: Currently not working because of next 13 beta
+"use client"
 
-import type { Metadata } from "next"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { Undo2 } from "lucide-react"
 
-export const metadata: Metadata = {
-  title: "Home",
-  description: "Welcome to Next.js",
-}
+import { Button } from "@/app/(components)/ui/button"
 
 export default function NotFound() {
-  //redirect("/404");
+  const router = useRouter()
+
+  const returnBack = () => {
+    router.replace("/")
+  }
+
   return (
     <>
-      {/*<h1>If u can see it something goes wrong</h1>*/}
-      <h2>The page you’re looking for doesn’t exist.</h2>
-      <p>
-        Want this to be your username?{" "}
-        <Link href={"/signup"}>Create free account</Link>.
-      </p>
+      {/*
+        No support for metadata in not-found.tsx yet
+        https://github.com/vercel/next.js/pull/47328#issuecomment-1488891093
+      */}
+      <title>Not Found</title>
+      <div
+        className={"flex h-full w-full flex-col items-center justify-center"}>
+        <h1 className={"p-12 text-9xl"}>404</h1>
+        <Button onClick={returnBack} className={"m-6"}>
+          <Undo2 className={"mr-3"} />
+          Go back
+        </Button>
+        <h2 className={"text-xl"}>
+          The page you’re looking for doesn’t exist.
+        </h2>
+        <p>
+          <span className={"p-2"}>Want to take it url?</span>
+          <Link href={"/login"} className={"underline"}>
+            Create free account
+          </Link>
+          .
+        </p>
+      </div>
     </>
   )
 }
