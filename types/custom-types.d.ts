3016d4ccdf12e79d753fb084ba54b086e80b5c34
Zeit241 <easywin.ru@gmail.com>
2023-07-17 23:15:31 +0300

    added type for prettifying types
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- types/custom-types.d.ts
+++ types/custom-types.d.ts
@@ -1,5 +1,9 @@
 export interface UserSession {
-    id: string
-    username: string
-    role: "USER" | "ADMIN"
+  id: string
+  username: string
+  role: "USER" | "ADMIN"
 }
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {}
