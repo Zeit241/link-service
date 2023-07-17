beb01eec3007b09ee927d1ba40be8ba1caec7afb
Zeit241 <easywin.ru@gmail.com>
2023-07-17 18:40:26 +0300

    added new share dialog
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/record.tsx
+++ src/app/(components)/record.tsx
@@ -1,20 +1,202 @@
 "use client"
 
-import { Link, Record } from "@prisma/client"
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { Link as LinkType, Record as RecordType } from "@prisma/client"
+import {
+  ChevronRight,
+  ExternalLink,
+  Facebook,
+  Flag,
+  Link as LinkIcon,
+  Mail,
+  MoreHorizontal,
+  Twitter,
+} from "lucide-react"
 
 import LinkItem from "@/app/(components)/link"
+import { Button } from "@/app/(components)/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/(components)/ui/dialog"
 
 type RecordProps = {
-  data: Record & { Link: Link[] }
-  links: Link[]
+  data: RecordType & { Link: LinkType[] }
+  links: LinkType[]
 }
 
 export default function RecordPage({ data, links }: RecordProps) {
+  const router = useRouter()
+  const [copy, setCopy] = useState<boolean>(false)
+  const [fullUrl, setFullUrl] = useState<string>("")
+
+  const shareOnTwitter = async (): Promise<void> => {
+    const url = `https://twitter.com/intent/tweet?text=Check%20out%20this%20link!%20-%20${fullUrl}`
+    await router.push(url)
+  }
+
+  const shareViaFacebook = async (): Promise<void> => {
+    const url = `https://www.facebook.com/sharer.php?u=${fullUrl}`
+    await router.push(url)
+  }
+
+  const shareViaEmail = async (): Promise<void> => {
+    const url = `mailto:?subject=%20Check%20out%20this%20link%20&body=%20Check%20out%20this%20link%20-%20${fullUrl}`
+    await router.push(url)
+  }
+
+  const shareViaOther = async (): Promise<void> => {
+    await window?.navigator?.share({
+      title: "Check out this link",
+      url: fullUrl,
+    })
+  }
+
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      setCopy(false)
+    }, 3000)
+    return () => clearTimeout(timerID)
+  }, [copy])
+
+  useEffect(() => {
+    setFullUrl(window?.location?.href)
+  }, [])
+
   return (
     <div
-      className={`flex h-full min-h-full w-full flex-col bg-background`}
+      className={`flex h-full min-h-full w-full flex-col items-center bg-background`}
       //style={{ backgroundColor: data.preferences.backgroundColor }}
     >
+      <div
+        className={
+          "flex h-20 w-[78%] min-w-[260px] max-w-[620px] flex-row  justify-end "
+        }>
+        <div className={"flex flex-row  items-center"}>
+          <Dialog modal={true}>
+            <DialogTrigger asChild>
+              <Button
+                className={"rounded-full bg-muted-foreground p-2"}
+                size={"icon"}
+                asChild>
+                <MoreHorizontal size={18} />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className={"w-full pl-2 pr-2"}>
+              <DialogHeader
+                className={"flex max-w-full items-center gap-2 p-0 "}>
+                <DialogTitle className={"mb-8"}>Share this link</DialogTitle>
+                <Button
+                  onClick={shareOnTwitter}
+                  variant={"ghost"}
+                  className={
+                    "flex w-full flex-row justify-between gap-2.5 p-4 pb-7 pt-7"
+                  }>
+                  <Twitter />
+                  <span
+                    className={"flex flex-1 flex-grow flex-wrap justify-start"}>
+                    Share on Twitter
+                  </span>
+                  <ChevronRight />
+                </Button>
+                <Button
+                  onClick={shareViaFacebook}
+                  variant={"ghost"}
+                  className={
+                    "flex w-full flex-row justify-between gap-2.5 p-4 pb-7 pt-7"
+                  }>
+                  <Facebook />
+                  <span
+                    className={"flex flex-1 flex-grow flex-wrap justify-start"}>
+                    Share via Facebook
+                  </span>
+                  <ChevronRight />
+                </Button>
+                <Button
+                  onClick={shareViaEmail}
+                  variant={"ghost"}
+                  className={
+                    "flex w-full flex-row justify-between gap-2.5 p-4 pb-7 pt-7"
+                  }>
+                  <Mail />
+                  <span
+                    className={"flex flex-1 flex-grow flex-wrap justify-start"}>
+                    Share via Email
+                  </span>
+                  <ChevronRight />
+                </Button>
+                <Button
+                  onClick={shareViaOther}
+                  variant={"ghost"}
+                  className={
+                    "flex w-full flex-row justify-between gap-2.5 p-4 pb-7 pt-7"
+                  }>
+                  <ExternalLink />
+                  <span
+                    className={"flex flex-1 flex-grow flex-wrap justify-start"}>
+                    More options
+                  </span>
+                  <ChevronRight />
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  className={
+                    "m-4 flex w-[90%] max-w-[90%] flex-row justify-between gap-2.5 border p-4 pb-7 pt-7"
+                  }
+                  onClick={async () => {
+                    setCopy(true)
+                    await window?.navigator?.clipboard?.writeText(fullUrl)
+                  }}>
+                  <LinkIcon />
+                  <span
+                    className={
+                      "flex flex-1 flex-grow flex-wrap justify-start overflow-hidden text-ellipsis whitespace-nowrap"
+                    }>
+                    {fullUrl}
+                  </span>
+                  <span className={copy ? "text-green-300" : ""}>
+                    {copy ? "Copied" : "Copy"}
+                  </span>
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  className={
+                    "mt-5 flex h-16 w-full flex-row items-center  justify-between gap-2.5 border-b border-t"
+                  }
+                  asChild>
+                  <Link href={`/report?id=${data.id}`} className={"flex"}>
+                    <Flag />
+                    <span
+                      className={
+                        "flex flex-1 flex-grow flex-wrap justify-start"
+                      }>
+                      Report this link
+                    </span>
+                    <ChevronRight />
+                  </Link>
+                </Button>
+                <>
+                  <h1 className={"text-xl font-bold"}>Create your own link</h1>
+                  <p className={"m-1"}>Share this link with your friends</p>
+                  <div className={"flex w-[65%] flex-col gap-2"}>
+                    <Button asChild>
+                      <Link href={"/login"}>Sign up for free</Link>
+                    </Button>
+                    <Button variant={"secondary"}>
+                      <Link href={"/"}>What&apos;s this</Link>
+                    </Button>
+                  </div>
+                </>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
       <nav className={"flex flex-col items-center p-8"}>
         <div
           className={
@@ -29,12 +211,10 @@ export default function RecordPage({ data, links }: RecordProps) {
       <section
         className={
           "m-0 flex h-full w-full flex-1 flex-col flex-wrap content-center items-center justify-start gap-4"
-        }
-        style={{
-          margin: "0 auto",
-        }}>
+        }>
         {links.map(
-          (link: Link) => link.enabled && <LinkItem key={link.id} link={link} />
+          (link: LinkType) =>
+            link.enabled && <LinkItem key={link.id} link={link} />
         )}
       </section>
       <div className={"mb-2 mt-12 flex items-center justify-center"}>
