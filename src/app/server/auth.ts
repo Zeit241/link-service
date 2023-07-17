312e099a6ebcba647c052d54168c237cee8614dc
Zeit241 <easywin.ru@gmail.com>
2023-07-18 01:49:41 +0300

    Reworked auth funcs
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

---
+++ src/app/server/auth.ts
@@ -0,0 +1,55 @@
+"use server"
+
+import { prisma } from "@/lib/database"
+
+import { BaseReturnType } from "../../../types/custom-types"
+import { Prisma, User } from ".prisma/client"
+
+type UserAuthFormProps = Pick<User, "username" | "password">
+
+export const SingUp = async (
+  data: UserAuthFormProps
+): Promise<BaseReturnType> => {
+  try {
+    await prisma.user.create({
+      data: {
+        ...data,
+        role: "USER",
+      },
+    })
+    return { status: 200, message: "User successfully created!" }
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        return {
+          status: 403,
+          message:
+            "There is a unique constraint violation, a new user cannot be created with this username.",
+        }
+      }
+    }
+  }
+  return { status: 500, message: "An unknown error occurred." }
+}
+
+type UserSignInProps = Pick<User, "username">
+type UserSignInResult = Pick<User, "id" | "username" | "password" | "role">
+
+//TODO: Find a way to throw error to the client
+
+export const SignIn = async (
+  data: UserSignInProps
+): Promise<UserSignInResult | null> => {
+  //try {
+  const user = await prisma?.user.findUnique({
+    where: data,
+    select: {
+      id: true,
+      username: true,
+      password: true,
+      role: true,
+    },
+  })
+  return user ? user : null
+  // } catch (e) {}
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,66 +1,58 @@
-import {PrismaAdapter} from "@next-auth/prisma-adapter"
-import {NextAuthOptions} from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { NextAuthOptions } from "next-auth"
 import CredentialProvider from "next-auth/providers/credentials"
 
-import {prisma} from "@/lib/database"
+import { prisma } from "@/lib/database"
+import { SignIn } from "@/app/server/auth"
 
-import {UserSession} from "../../types/custom-types"
+import { UserSession } from "../../types/custom-types"
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialProvider({
-            type: "credentials",
-            name: "credentials",
-            credentials: {
-                username: {type: "text"},
-                password: {type: "password"},
-            },
-            async authorize(credentials) {
-                if (!credentials?.username || !credentials?.password) return null
-                const {username, password} = credentials
-                let user = await prisma?.user.findUnique({
-                    where: {
-                        username: username,
-                    },
-                    select: {
-                        id: true,
-                        username: true,
-                        password: true,
-                        role: true,
-                    },
-                })
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialProvider({
+      type: "credentials",
+      name: "credentials",
+      credentials: {
+        username: { type: "text" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) return null
+        const { username, password } = credentials
+        const user = await SignIn({ username })
+        if (!user) return null
 
-                if (!user) return null
+        //TODO: ADD PASSWORD VALIDATION
 
-                //const isPassportValid = await bcrypt.compare(password, user.password);
-                //if (!isPassportValid) return null;
-                return user
-            },
-        }),
-    ],
-    session: {
-        strategy: "jwt",
-        maxAge: 60 * 60 * 24,
+        //const isPassportValid = await bcrypt.compare(password, user.password);
+        //if (!isPassportValid) return null;
+        return user
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      return !!user
     },
-    callbacks: {
-        async signIn({user}) {
-            return !!user
-        },
-        jwt: async ({token, user}) => {
-            user ? (token.user = <UserSession>user) : null
-            return token
-        },
-        session: async ({session, token}) => {
-            if (token) {
-                session.user = token.user
-            }
-            return session
-        },
+    jwt: async ({ token, user }) => {
+      user ? (token.user = <UserSession>user) : null
+      return token
     },
-    debug: process.env.NODE_ENV === "production",
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/login",
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user = token.user
+      }
+      return session
     },
+  },
+  debug: process.env.NODE_ENV === "production",
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
 }
