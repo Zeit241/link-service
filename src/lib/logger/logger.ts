822cdbc1d824229e076651332b11ec43f509b6a4
Zeit241 <easywin.ru@gmail.com>
2023-07-18 01:31:50 +0300

    added winston package for logging
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

---
+++ src/lib/logger/logger.ts
@@ -0,0 +1,11 @@
+const { createLogger, transports, format } = require("winston")
+
+export const logger = createLogger({
+  level: "info",
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "error.log", level: "error" }),
+    new transports.File({ filename: "combined.log" }),
+  ],
+})
