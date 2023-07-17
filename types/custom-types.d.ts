0744eb72c202c26720f2ef9887a3490701fa4f68
Zeit241 <easywin.ru@gmail.com>
2023-07-18 01:50:22 +0300

    Improved types
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- types/custom-types.d.ts
+++ types/custom-types.d.ts
@@ -1,8 +1,8 @@
-export interface UserSession {
-  id: string
-  username: string
-  role: "USER" | "ADMIN"
-}
+import { User } from "@prisma/client"
+
+export type UserSession = Pick<User, "id" | "username" | "role">
+
+export type BaseReturnType = { status: number; message: string }
 
 export type Prettify<T> = {
   [K in keyof T]: T[K]
