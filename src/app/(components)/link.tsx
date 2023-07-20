6de54d36d7031dcab547727cbcb3a20807d0ed9d
Zeit241 <easywin.ru@gmail.com>
2023-07-20 21:16:31 +0300

    cleaned and fixed link component
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/link.tsx
+++ src/app/(components)/link.tsx
@@ -1,20 +1,19 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { usePathname, useRouter } from "next/navigation"
+//import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 import { Link } from "@prisma/client"
 
 type LinkItemProps = { link: Link }
 
 export default function LinkItem({ link }: LinkItemProps): JSX.Element {
-  const [isHovered, setIsHovered] = useState<boolean>(false)
-  const [isIframe, setIsIframe] = useState<boolean>(false)
+  // const [isHovered, setIsHovered] = useState<boolean>(false)
+  //const [isIframe, setIsIframe] = useState<boolean>(false)
+  //const pathname = usePathname()
   const router = useRouter()
-  const pathname = usePathname()
-  const redirect = async (): Promise<void> => {
-    await router.push(link.url, {
-      replace: true,
-    })
+
+  const redirect = (): void => {
+    router.replace(link.url)
   }
 
   const sendStatistic = async (): Promise<void> => {
@@ -28,12 +27,12 @@ export default function LinkItem({ link }: LinkItemProps): JSX.Element {
         recordId: link.recordId,
         shared: null,
       }),
-    }).then(async () => await redirect())
+    }).then(() => redirect())
   }
 
-  useEffect(() => {
-    setIsIframe(pathname.startsWith("/dashboard"))
-  }, [pathname])
+  // useEffect(() => {
+  //   setIsIframe(pathname.startsWith("/dashboard"))
+  // }, [pathname])
 
   return (
     <div
@@ -41,16 +40,18 @@ export default function LinkItem({ link }: LinkItemProps): JSX.Element {
       className={
         "flex h-auto w-[75%] min-w-[260px] max-w-[560px]  cursor-pointer content-center bg-muted p-2.5 pl-5 pr-5 transition-all duration-150 hover:scale-105"
       }
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}>
+      // onMouseEnter={() => setIsHovered(true)}
+      // onMouseLeave={() => setIsHovered(false)}
+    >
       <div className={"flex w-full content-center items-center justify-center"}>
         <div className={"flex w-full flex-col flex-wrap items-center"}>
           {link.customImage?.enabled && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img src={link.customImage.image!} alt="Link Icon" />
           )}
           <h1
             className={
-              "mr-[-30px] flex flex-wrap break-all text-center font-semibold sm:text-base "
+              " flex flex-wrap break-all text-center font-semibold sm:text-base "
             }>
             {link.name}
           </h1>
