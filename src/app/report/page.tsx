e0e4257a5b5b4e4fbef3f0c09e145ae063e7b615
Zeit241 <easywin.ru@gmail.com>
2023-07-20 22:09:30 +0300

    added check for existing record and added ;loader
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { notFound, useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Loader2 } from "lucide-react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
@@ -25,21 +27,31 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/app/(components)/ui/select"
+import { GetRecordCount } from "@/app/actions/record"
 import { SendReport } from "@/app/actions/report"
 
-//TODO: ADD CHECK FOR LINK EXISTS
 export default function Report({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  // const [data, setData] = useState<{
-  //
-  // }>()
-  // useEffect(()=>{
-  //   fetch("")
-  // }, [])
   const { id } = searchParams
+  const [valid, setValid] = useState(true)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(true)
+    if (!id || !searchParams || typeof id !== "string") return setValid(false)
+
+    GetRecordCount({ id: id as string }).then((data) => {
+      if (data.status !== 200 || !data?.record) {
+        setValid(false)
+      }
+      setTimeout(() => {
+        setLoading(false)
+      }, 10000)
+    })
+  }, [id, searchParams])
+  if (!valid) notFound()
   const { toast } = useToast()
   const router = useRouter()
   const formSchema = z.object({
@@ -54,8 +66,6 @@ export default function Report({
       message: "Select a reason to report",
     },
   })
-
-  if (!id || !searchParams || typeof id !== "string") return notFound()
 
   async function onSubmit(data: z.infer<typeof formSchema>) {
     await SendReport({ recordId: id as string, text: data.message })
@@ -73,60 +83,69 @@ export default function Report({
       <SessionProviderWrapper>
         <Navbar />
       </SessionProviderWrapper>
-      <div className={"flex h-full w-full flex-col items-center justify-start"}>
-        <h1
-          className={
-            "mt-10  items-center justify-center overflow-hidden text-ellipsis break-all text-center text-4xl font-bold tracking-wider"
-          }>
-          Report for <p>#{id}</p>
-        </h1>
-        <p className={"w-full p-5 text-center sm:w-3/5"}>
-          We understand that you are having problems with the content on our
-          website. We take such situations very seriously and appreciate your
-          appeal. Please fill out the form below to describe in detail the
-          problem you are facing. We will review your complaint as soon as
-          possible and take the necessary actions.
-        </p>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex w-5/6 flex-col  justify-end space-y-6 sm:w-3/5">
-            <FormField
-              control={form.control}
-              name="message"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Reason for complaint</FormLabel>
-                  <Select onValueChange={field.onChange}>
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a reason to report" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="copyright">
-                        Copyright infringement
-                      </SelectItem>
-                      <SelectItem value="spam">
-                        Spam or deceptive practices
-                      </SelectItem>
-                      <SelectItem value="harmful">
-                        Malicious or harmful content
-                      </SelectItem>
-                      <SelectItem value="other">Other</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <FormDescription></FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className={"w-24"}>
-              Submit
-            </Button>
-          </form>
-        </Form>
-      </div>
+
+      {loading ? (
+        <div
+          className={"flex h-full w-full flex-col items-center justify-center"}>
+          <Loader2 className={"animate-spin"} />
+        </div>
+      ) : (
+        <div
+          className={"flex h-full w-full flex-col items-center justify-start"}>
+          <h1
+            className={
+              "mt-10  items-center justify-center overflow-hidden text-ellipsis break-all text-center text-4xl font-bold tracking-wider"
+            }>
+            Report for <p>#{id}</p>
+          </h1>
+          <p className={"w-full p-5 text-center sm:w-3/5"}>
+            We understand that you are having problems with the content on our
+            website. We take such situations very seriously and appreciate your
+            appeal. Please fill out the form below to describe in detail the
+            problem you are facing. We will review your complaint as soon as
+            possible and take the necessary actions.
+          </p>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex w-5/6 flex-col  justify-end space-y-6 sm:w-3/5">
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Reason for complaint</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a reason to report" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="copyright">
+                          Copyright infringement
+                        </SelectItem>
+                        <SelectItem value="spam">
+                          Spam or deceptive practices
+                        </SelectItem>
+                        <SelectItem value="harmful">
+                          Malicious or harmful content
+                        </SelectItem>
+                        <SelectItem value="other">Other</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className={"w-24"}>
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+      )}
     </>
   )
 }
