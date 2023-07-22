411cee70f5abb7162b7f79fb4ff8f6fbd4011de4
Zeit241 <easywin.ru@gmail.com>
2023-07-22 23:04:26 +0300

    Added domain for images optimization
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-        serverActions: true,
-        serverComponentsExternalPackages: ["@prisma/client"],
-    },
+  experimental: {
+    appDir: true,
+    serverActions: true,
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  images: {
+    domains: ["api.qrserver.com"],
+  },
 }
 
 module.exports = nextConfig
