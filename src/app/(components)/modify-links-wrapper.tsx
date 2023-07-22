c84ce44f6ed8183d504a767d8462f494c14bd78c
Zeit241 <easywin.ru@gmail.com>
2023-07-22 23:11:05 +0300

    Added new version of modify wrapper
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/modify-links-wrapper.tsx
+++ src/app/(components)/modify-links-wrapper.tsx
@@ -1,191 +1,45 @@
 "use client"
 
-import * as punycode from "punycode"
 import * as React from "react"
-import { useState } from "react"
-import { useAutoAnimate } from "@formkit/auto-animate/react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Link } from "@prisma/client"
-import { Loader2, Plus, X } from "lucide-react"
-import { Controller, useForm } from "react-hook-form"
-import { z } from "zod"
 
-import { useStore } from "@/lib/store/store"
-import ModifyLinkCard from "@/app/(components)/modify-link-card"
-import RecordPage from "@/app/(components)/record"
-import { Button } from "@/app/(components)/ui/button"
-import { Card } from "@/app/(components)/ui/card"
-import { Form, FormField, FormItem } from "@/app/(components)/ui/form"
-import { Input } from "@/app/(components)/ui/input"
-import { Label } from "@/app/(components)/ui/label"
-import { CreateLink } from "@/app/actions/link"
+import CheckAnalytics from "@/app/(components)/check-analytics"
+import EditAppearance from "@/app/(components)/edit-appearance"
+import EditLinks from "@/app/(components)/edit-links"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/(components)/ui/tabs"
 
-const createNewLink = z.object({
-  url: z.string().refine(
-    (value) => {
-      const pattern = new RegExp(
-        "^(https?:\\/\\/)?" + // protocol
-          "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[-*a-z\\d]{2,}|" + // domain name
-          "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
-          "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-          "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-          "(\\#[-a-z\\d_]*)?$", // fragment locator
-        "i"
-      )
-      return pattern.test(punycode.toASCII(value))
-    },
-    { message: "Invalid URL" }
-  ),
-})
 export default function ModifyLinksWrapper() {
-  const { links, record, add_link } = useStore()
-  const [isAddCardOpen, setIsAddCardOpen] = useState<boolean>(false)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [parent] = useAutoAnimate()
-
-  async function onSubmit(value: z.infer<typeof createNewLink>) {
-    setIsLoading(true)
-    const res = await CreateLink({
-      url: value.url,
-      name: value.url,
-      recordId: record.id,
-      order: record?.Link?.length,
-    })
-
-    if (links !== undefined && links.length > 0) {
-      if (res.link) {
-        add_link(res.link)
-      }
-    }
-    setIsLoading(false)
-    setIsAddCardOpen(false)
-  }
-
-  const form = useForm<z.infer<typeof createNewLink>>({
-    resolver: zodResolver(createNewLink),
-    mode: "onChange",
-    reValidateMode: "onChange",
-    defaultValues: {
-      url: "",
-    },
-  })
-
   return (
-    <>
-      <div
-        className={"flex w-full flex-row items-start justify-center p-8 pb-0"}>
-        <div className={"flex flex-1 flex-wrap items-center  justify-center"}>
-          <div className={"flex w-full flex-col items-center gap-5 pb-4 pt-4"}>
-            {!isAddCardOpen && (
-              <Button
-                variant="secondary"
-                className="max-w-xl p-4 min-[320px]:w-[95%]"
-                onClick={() => {
-                  setIsAddCardOpen(true)
-                }}>
-                <Plus /> Add new link
-              </Button>
-            )}
-            {isAddCardOpen && (
-              <Card
-                className={
-                  "flex max-w-xl flex-col gap-4 p-4 min-[320px]:w-[95%]"
-                }>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8">
-                    <div className="">
-                      <FormField
-                        control={form.control}
-                        name="url"
-                        render={({ field }) => (
-                          <FormItem>
-                            <div
-                              className={
-                                "flex w-full flex-row items-center justify-between"
-                              }>
-                              <Label htmlFor={"url"}>Enter URL</Label>
-                              <Button
-                                disabled={isLoading}
-                                variant="ghost"
-                                size="icon"
-                                onClick={() => {
-                                  setIsAddCardOpen(false)
-                                }}>
-                                <X size={18} />
-                              </Button>
-                            </div>
-                            <div className={"flex flex-row items-center gap-3"}>
-                              <Controller
-                                name="url"
-                                control={form.control}
-                                render={({ field: { onChange } }) => (
-                                  <>
-                                    <Input
-                                      {...field}
-                                      id="url"
-                                      placeholder="Username"
-                                      type="text"
-                                      autoCapitalize="none"
-                                      autoComplete="none"
-                                      autoCorrect="off"
-                                      spellCheck={"false"}
-                                      onChange={onChange}
-                                      className={`${isLoading ? "hidden" : ""}`}
-                                    />
-                                  </>
-                                )}
-                              />
-                              <Button
-                                disabled={
-                                  !!form.formState.errors.url?.message ||
-                                  isLoading
-                                }
-                                className={isLoading ? "w-full" : ""}
-                                type={"submit"}>
-                                {isLoading ? (
-                                  <Loader2 className={"animate-spin"} />
-                                ) : (
-                                  "Add"
-                                )}
-                              </Button>
-                            </div>
-                          </FormItem>
-                        )}
-                      />
-                    </div>
-                  </form>
-                </Form>
-              </Card>
-            )}
-            <div
-              ref={parent}
-              className={
-                " flex  h-full w-full flex-col items-center justify-center gap-4"
-              }>
-              {links.map((link: Link, index: number, array: Link[]) => (
-                <div className={"max-w-xl min-[300px]:w-[95%]"} key={link.id}>
-                  <ModifyLinkCard
-                    link={link}
-                    index={index}
-                    max={array.length}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={
-            "sticky top-[22%] mr-6 hidden h-[620px] w-[320px] min-w-[320px] overflow-hidden rounded-[50px] border-[15px] md:block lg:block xl:block"
-          }>
-          <div className={"h-full w-full overflow-x-hidden "}>
-            <RecordPage data={record} links={links} />
-          </div>
-        </div>
-      </div>
-    </>
+    <div
+      className={
+        "flex w-full flex-row items-start justify-center p-2 pb-0 sm:p-8"
+      }>
+      <Tabs defaultValue="links" className="w-full gap-3">
+        <TabsList className={" h-12 w-full"}>
+          <TabsTrigger value="links" className={"w-[35%] p-3"}>
+            Links
+          </TabsTrigger>
+          <TabsTrigger value="appearance" className={"w-[35%] p-3"}>
+            Appearance
+          </TabsTrigger>
+          <TabsTrigger value="analytics" className={"w-[35%] p-3"}>
+            Analytics
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="links">
+          <EditLinks />
+        </TabsContent>
+        <TabsContent value="appearance">
+          <EditAppearance />
+        </TabsContent>
+        <TabsContent value="analytics">
+          <CheckAnalytics />
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
