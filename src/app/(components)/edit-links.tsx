fe2eac4896939b81a57e001c665d4e7816615584
Zeit241 <easywin.ru@gmail.com>
2023-07-25 18:42:49 +0300

    added  alert if project is disabled and improved mobile view
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/edit-links.tsx
+++ src/app/(components)/edit-links.tsx
@@ -2,11 +2,13 @@
 
 import * as punycode from "punycode"
 import * as React from "react"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import autoAnimate from "@formkit/auto-animate"
 import { useAutoAnimate } from "@formkit/auto-animate/react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Link } from "@prisma/client"
 import {
+  AlertTriangle,
   Loader2,
   PanelBottomClose,
   PanelBottomOpen,
@@ -19,6 +21,11 @@ import { z } from "zod"
 import { useStore } from "@/lib/store/store"
 import ModifyLinkCard from "@/app/(components)/modify-link-card"
 import RecordPage from "@/app/(components)/record"
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/app/(components)/ui/alert"
 import { Button } from "@/app/(components)/ui/button"
 import { Card } from "@/app/(components)/ui/card"
 import { Form, FormField, FormItem } from "@/app/(components)/ui/form"
@@ -48,9 +55,19 @@ export default function EditLinks(): JSX.Element {
   const [isAddCardOpen, setIsAddCardOpen] = useState<boolean>(false)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isMobilePreviewOpen, setIsMobilePreviewOpen] = useState<boolean>(false)
+  const [isAlertOpen, setIsAlertOpen] = useState<boolean>(!record.enabled)
   const [list] = useAutoAnimate()
   const [card] = useAutoAnimate()
-  const [mobilePreview] = useAutoAnimate()
+  const mobilePreview = useRef<HTMLDivElement>(null)
+  const alert = useRef(null)
+
+  useEffect(() => {
+    alert.current && autoAnimate(alert.current)
+  }, [alert])
+
+  useEffect(() => {
+    mobilePreview.current && autoAnimate(mobilePreview.current, {})
+  }, [mobilePreview])
 
   const getSiteTitle = async (url: string) => {
     console.log(url)
@@ -70,7 +87,7 @@ export default function EditLinks(): JSX.Element {
       url: value.url,
       name: value.url,
       recordId: record.id,
-      order: record.Link.length,
+      order: links.length,
     })
     await getSiteTitle(value.url)
     form.reset()
@@ -89,6 +106,29 @@ export default function EditLinks(): JSX.Element {
   })
   return (
     <>
+      <div ref={alert}>
+        {isAlertOpen && (
+          <Alert
+            className={"me-auto ms-auto w-5/6 sm:w-4/5"}
+            variant={"destructive"}>
+            <AlertTriangle className="h-8 w-8 pr-2" />
+            <AlertTitle
+              className={
+                "ml-2 flex cursor-pointer flex-row items-center justify-between"
+              }>
+              <span>Attention!</span>
+              <X size={18} onClick={() => setIsAlertOpen(false)} />
+            </AlertTitle>
+            <AlertDescription className={"ml-2"}>
+              <p>
+                Your project is disabled and other users don&apos;t see it. You
+                can enable it in the dashboard.
+              </p>
+            </AlertDescription>
+          </Alert>
+        )}
+      </div>
+
       <div
         className={
           "relative flex w-full flex-row items-start justify-center p-8 pb-0 min-[100px]:p-2"
@@ -190,41 +230,46 @@ export default function EditLinks(): JSX.Element {
                 " flex  h-full w-full flex-col items-center justify-center gap-4"
               }>
               {links.map((link: Link, index: number, array: Link[]) => (
-                <div
-                  className={"w-[640px] max-w-xl min-[300px]:w-[95%]"}
-                  key={link.id}>
-                  <ModifyLinkCard
-                    link={link}
-                    index={index}
-                    max={array.length}
-                  />
-                </div>
+                <ModifyLinkCard
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  max={array.length - 1}
+                />
               ))}
             </div>
           </div>
         </div>
-
         <div
           className={
-            "sticky top-[22%] mr-6 hidden h-[620px] w-[320px] min-w-[320px] overflow-hidden rounded-[50px] border-[15px] min-[890px]:block lg:m-16"
+            "sticky top-[28%] ml-4 mr-4 hidden h-[620px] w-[320px] min-w-[320px] overflow-hidden rounded-[50px] border-[15px] min-[890px]:block lg:m-16"
           }>
-          <div
-            className={"h-full w-full overflow-x-hidden "}
-            ref={mobilePreview}>
+          <div className={"h-full w-full overflow-x-hidden "}>
             <RecordPage data={record} links={links.filter((l) => l.enabled)} />
           </div>
         </div>
-        {isMobilePreviewOpen && (
-          <div
-            className={
-              "fixed bottom-[0%] right-[0%] h-full w-full overflow-y-scroll bg-background min-[890px]:hidden"
-            }>
-            <RecordPage data={record} links={links.filter((l) => l.enabled)} />
-          </div>
-        )}
+        <div ref={mobilePreview} className={" min-[890px]:hidden"}>
+          {isMobilePreviewOpen && (
+            <>
+              <div
+                className={
+                  "fixed right-[0%] top-[0%] h-screen w-screen bg-background opacity-60"
+                }></div>
+              <div
+                className={
+                  "fixed right-[0%] top-[5%] h-[95%] w-full overflow-y-scroll rounded-t-[30px] border-t-[15px] bg-background"
+                }>
+                <RecordPage
+                  data={record}
+                  links={links.filter((l) => l.enabled)}
+                />
+              </div>
+            </>
+          )}
+        </div>
         <div
           className={
-            "fixed bottom-[5%] right-[50%] translate-x-[+50%] min-[890px]:hidden"
+            "fixed bottom-[5%] right-[50%] translate-x-[+50%] opacity-80 min-[890px]:hidden"
           }>
           <Button
             className={"flex w-40 flex-row gap-3 p-8 pb-6 pt-6"}
