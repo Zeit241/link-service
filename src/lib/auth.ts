34d39b6386aa0f762c28d026a4464e46e84804cd
Zeit241 <easywin.ru@gmail.com>
2023-07-27 22:49:50 +0300

    added support of Google and GitHub providers
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import bcrypt from "bcrypt"
 import { NextAuthOptions } from "next-auth"
 import CredentialProvider from "next-auth/providers/credentials"
+import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 import { prisma } from "@/lib/database"
 import { SignIn } from "@/app/actions/auth"
@@ -23,13 +26,46 @@ export const authOptions: NextAuthOptions = {
         const user = await SignIn({ username })
         if (!user) return null
 
-        //TODO: ADD PASSWORD VALIDATION
-        //const isPassportValid = await bcrypt.compare(password, user.password);
-        //if (!isPassportValid) return null;
+        const isPassportValid = await bcrypt.compare(password, user.password!)
+        if (!isPassportValid) return null
         return {
           id: user.id,
           username: user.username,
           role: user.role,
+        }
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env?.GITHUB_CLIENT_ID!,
+      clientSecret: process.env?.GITHUB_CLIENT_SECRET!,
+      name: "GitHub",
+      profile(profile) {
+        return {
+          id: profile.id,
+          email: profile.email,
+          image: profile.avatar_url,
+          name: profile.login,
+          status: "ACTIVE",
+          verified: false,
+          role: "USER",
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      profile(profile) {
+        console.log("profile", profile)
+        return {
+          id: profile.sub,
+          email: profile.email,
+          emailVerified: profile.email_verified,
+          image: profile.picture,
+          name: profile.name,
+          username: profile.email.split("@")[0],
+          status: "ACTIVE",
+          verified: false,
+          role: "USER",
         }
       },
     }),
@@ -48,7 +84,10 @@ export const authOptions: NextAuthOptions = {
     },
     session: async ({ session, token }) => {
       if (token) {
-        session.user = token.user
+        session.user = {
+          ...token.user,
+          username: token.user?.username || token.user?.name || "John Doe",
+        }
       }
       return session
     },
