5263a0795025ec8f8da8c76f70e88bbce2cd6805
Zeit241 <easywin.ru@gmail.com>
2023-07-27 22:48:54 +0300

    moved request to database to record file
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/record-wrapper.tsx
+++ src/app/(components)/record-wrapper.tsx
@@ -1,45 +1,27 @@
-import {redirect} from "next/navigation"
-import {Record} from "@prisma/client"
-import {getServerSession} from "next-auth"
+import { redirect } from "next/navigation"
+import { Record } from "@prisma/client"
+import { getServerSession } from "next-auth"
 
-import {authOptions} from "@/lib/auth"
-import {prisma} from "@/lib/database"
+import { authOptions } from "@/lib/auth"
 import CreateCardBtn from "@/app/(components)/add-card"
 import RecordCard from "@/app/(components)/record-card"
+import { getLinkAndStatisticCount } from "@/app/actions/record"
 
-type customType = Record & { Link: { id: string }[] } & {
-    Statistic: { id: string }[]
-}
+type customType = Record & { _count: { Link: number; Statistic: number } }
 
-export default async function RecordWrapper() {
-    const session = await getServerSession(authOptions)
-    if (session) {
-        const data = await prisma.record.findMany({
-            where: {
-                userId: session?.user?.id,
-            },
-            include: {
-                Link: {
-                    include: {
-                        _count: true,
-                    },
-                },
-                Statistic: {
-                    select: {
-                        id: true,
-                    },
-                },
-            },
-        })
-        return (
-            <div className={"flex flex-row flex-wrap gap-4"}>
-                {data.map((record: customType) => (
-                    <RecordCard key={record.id} record={record}/>
-                ))}
-                <CreateCardBtn id={session?.user?.id}/>
-            </div>
-        )
-    } else {
-        return redirect("/login")
-    }
+export default async function RecordWrapper(): Promise<Awaited<JSX.Element>> {
+  const session = await getServerSession(authOptions)
+  if (session) {
+    const { data } = await getLinkAndStatisticCount({ id: session?.user?.id })
+    return (
+      <div className={"flex flex-row flex-wrap gap-4"}>
+        {data?.map((record: customType) => (
+          <RecordCard key={record.id} record={record} />
+        ))}
+        <CreateCardBtn id={session?.user?.id} />
+      </div>
+    )
+  } else {
+    return redirect("/login")
+  }
 }
