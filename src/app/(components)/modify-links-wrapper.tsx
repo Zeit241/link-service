224b758dfc2ba358eaa087ce3eda60e76116cccf
Zeit241 <easywin.ru@gmail.com>
2023-07-29 20:32:57 +0300

    Added function for setting tab while refresh page
    Signed-off-by: Zeit241 <easywin.ru@gmail.com>

--- src/app/(components)/modify-links-wrapper.tsx
+++ src/app/(components)/modify-links-wrapper.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import * as React from "react"
+import { useEffect, useState } from "react"
 
+import { useLocationStore } from "@/lib/store/store"
 import CheckAnalytics from "@/app/(components)/check-analytics"
 import EditAppearance from "@/app/(components)/edit-appearance"
 import EditLinks from "@/app/(components)/edit-links"
@@ -13,20 +15,35 @@ import {
 } from "@/app/(components)/ui/tabs"
 
 export default function ModifyLinksWrapper() {
+  const [currentTab, setCurrentTab] = useState("links")
+  const { location, setLocation } = useLocationStore()
+  useEffect(() => {
+    setCurrentTab(location)
+  }, [location])
+
   return (
     <div
       className={
         "flex w-full flex-row items-start justify-center p-2 pb-0 sm:p-8"
       }>
-      <Tabs defaultValue="links" className="w-full gap-3">
+      <Tabs value={currentTab} className="w-full gap-3">
         <TabsList className={" h-12 w-full"}>
-          <TabsTrigger value="links" className={"w-[35%] p-3"}>
+          <TabsTrigger
+            value="links"
+            className={"w-[35%] p-3"}
+            onClick={() => setLocation("links")}>
             Links
           </TabsTrigger>
-          <TabsTrigger value="appearance" className={"w-[35%] p-3"}>
+          <TabsTrigger
+            value="appearance"
+            className={"w-[35%] p-3"}
+            onClick={() => setLocation("appearance")}>
             Appearance
           </TabsTrigger>
-          <TabsTrigger value="analytics" className={"w-[35%] p-3"}>
+          <TabsTrigger
+            value="analytics"
+            className={"w-[35%] p-3"}
+            onClick={() => setLocation("analytics")}>
             Analytics
           </TabsTrigger>
         </TabsList>
